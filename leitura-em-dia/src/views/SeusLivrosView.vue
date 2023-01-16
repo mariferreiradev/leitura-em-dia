8a956503341b1466e33bfbad05dfdbbe31c3eca0
Mariana <mariferreira.dev@gmail.com>
2023-01-16 19:34:44 -0300

    add content to seus livros

--- leitura-em-dia/src/views/SeusLivrosView.vue
+++ leitura-em-dia/src/views/SeusLivrosView.vue
@@ -1,3 +1,15 @@
 <template>
-    
+    <div>
+        <button>Adicionar livro</button>
+        <div>
+            <div>
+                <input type="text" placeholder="Buscar por"/><button>Lupa</button>
+            </div>
+            <div>
+                <button><img src="" alt="">Filtro</button>
+            </div>
+        </div>
+        <p>A bela adormecida</p><div><span>Concluído</span></div>
+        <p>Branca de neve</p><div><span>Lendo</span></div>
+        </div>
 </template>
