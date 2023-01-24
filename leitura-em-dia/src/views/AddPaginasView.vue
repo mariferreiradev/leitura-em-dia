c1039ef5e4ffe4b86be68fe690c2b1f1ca9ca6f6
Mariana <mariferreira.dev@gmail.com>
2023-01-23 21:45:53 -0300

    added possibility to add pages

--- leitura-em-dia/src/views/AddPaginasView.vue
+++ leitura-em-dia/src/views/AddPaginasView.vue
@@ -1,3 +1,17 @@
 <template>
-    oi
+    <div>
+        <label for="">Escolha o livro que deseja concluir:</label>
+        <select name="" id="">
+            <option value="">Teste</option>
+        </select>
+    </div>
+    <div>
+        <label for="">Digite quantidade de paginas:</label>
+        <input type="text" />
+    </div>
+    <div>
+        <label for="">Digite quantidade de horas lidas:</label>
+        <input type="time" />
+    </div>
+    <button>Salvar</button>
 </template>
